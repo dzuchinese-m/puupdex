a2ff3cfe1d4858bebe93d1867c319cb30335a75d
Dzu <angus-trinh@protonmail.com>
2025-06-04 09:07:53 +0000

    update 2.3

--- pyrebaseConfig.py
+++ pyrebaseConfig.py
@@ -3,7 +3,7 @@ import pyrebase
 config = {
   "apiKey": "AIzaSyCNO00Bk8tIsZW-OcPMAFbJF-ONvAvZb2o",
   "authDomain": "fir-f254b.firebaseapp.com",
-  "databaseURL": "https://fir-f254b.firebaseio.com",
+  "databaseURL": "https://fir-f254b-default-rtdb.asia-southeast1.firebasedatabase.app",
   "storageBucket": "fir-f254b.appspot.com"
 }
 
